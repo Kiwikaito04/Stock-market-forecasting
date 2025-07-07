bca4ac96f13096f4c69a85df74abd2f2c500b6b6
Kiwikaito <nguyenphamdieu41@gmail.com>
2025-07-07 19:24:18 +0700

    đổi tên hàm create_label_Intraday, thêm hàm create_label_NextDay và create_stock_data_NextDay_1f

--- src/features.py
+++ src/features.py
@@ -15,7 +15,7 @@ def scalar_normalize(train_data, test_data):
     test_data[:, 2:-2] = scaler.transform(test_data[:, 2:-2])
 
 # perc=[0.5, 0.5] chia tỉ lệ 50% 50% (một nửa rank cao nhất sẽ label 1) (nửa còn lại label 0)
-def create_label(df_open, df_close, perc=[0.5, 0.5]):
+def create_label_Intraday(df_open, df_close, perc=[0.5, 0.5]):
 
     # if not np.all(df_close.iloc[:, 0] == df_open.iloc[:, 0]):
     if not np.all(df_close['Date'] == df_open['Date']):
@@ -29,6 +29,13 @@ def create_label(df_open, df_close, perc=[0.5, 0.5]):
         lambda x: pd.qcut(x.rank(method='first'), perc, labels=False), axis=1)
 
     return label[1:]  # bỏ ngày đầu tiên vì không có giá trị trước đó
+
+def create_label_NextDay(df_close, perc=[0.5, 0.5]):
+    perc = [0.] + list(np.cumsum(perc))
+    label = df_close.iloc[:, 1:].pct_change(fill_method=None)[1:].apply(
+        lambda x: pd.qcut(x.rank(method='first'), perc, labels=False), axis=1)
+
+    return label
 
 def create_stock_data_Intraday_3f(df_open, df_close, label, ticker: str, test_year: int, window: int = 240):
     df = pd.DataFrame()
@@ -78,6 +85,32 @@ def create_stock_data_Intraday_1f(df_open, df_close, label, ticker: str, test_ye
                    axis=1)
 
     df['IntraR-future'] = daily_change.shift(-1)
+    df['label'] = label[ticker].values.tolist() + [np.nan]
+    df['Month'] = df['Date'].str[:7]
+
+    df = df.dropna()
+    df['Year'] = df['Month'].str[:4].astype(int)
+
+    train = df[df['Year'] < test_year].drop(columns=['Month', 'Year'])
+    test = df[df['Year'] == test_year].drop(columns=['Month', 'Year'])
+    return np.array(train), np.array(test)
+
+def create_stock_data_NextDay_1f(df_close, label, ticker: str, test_year: int, window: int = 240):
+    df = pd.DataFrame()
+    df['Date'] = df_close['Date']
+    df['Name'] = ticker
+
+    daily_change = df_close[ticker].pct_change()
+
+    # Tạo dict cho từng nhóm đặc trưng
+    intra_features = {f'R{k}': daily_change.shift(k) for k in range(window)[::-1]}
+
+    # Gộp tất cả vào DataFrame
+    df = pd.concat([df,
+                    pd.DataFrame(intra_features)],
+                   axis=1)
+
+    df['R-future'] = daily_change.shift(-1)
     df['label'] = label[ticker].values.tolist() + [np.nan]
     df['Month'] = df['Date'].str[:7]
 
