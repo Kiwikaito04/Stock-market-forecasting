085d55a0be1ef0dde40aa286193840bd8e33e30c
Kiwikaito <nguyenphamdieu41@gmail.com>
2025-07-09 10:48:34 +0700

    đổi tên hàm để phân biệt LSTM và RF

--- src/features.py
+++ src/features.py
@@ -15,9 +15,7 @@ def scalar_normalize(train_data, test_data):
     test_data[:, 2:-2] = scaler.transform(test_data[:, 2:-2])
 
 # perc=[0.5, 0.5] chia tỉ lệ 50% 50% (một nửa rank cao nhất sẽ label 1) (nửa còn lại label 0)
-def create_label_Intraday(df_open, df_close, perc=[0.5, 0.5]):
-
-    # if not np.all(df_close.iloc[:, 0] == df_open.iloc[:, 0]):
+def create_label_LSTM_Intraday(df_open, df_close, perc=[0.5, 0.5]):
     if not np.all(df_close['Date'] == df_open['Date']):
         raise ValueError('Date index mismatch')
 
@@ -30,14 +28,14 @@ def create_label_Intraday(df_open, df_close, perc=[0.5, 0.5]):
 
     return label[1:]  # bỏ ngày đầu tiên vì không có giá trị trước đó
 
-def create_label_NextDay(df_close, perc=[0.5, 0.5]):
+def create_label_LSTM_NextDay(df_close, perc=[0.5, 0.5]):
     perc = [0.] + list(np.cumsum(perc))
     label = df_close.iloc[:, 1:].pct_change(fill_method=None)[1:].apply(
         lambda x: pd.qcut(x.rank(method='first'), perc, labels=False), axis=1)
 
     return label
 
-def create_stock_data_Intraday_3f(df_open, df_close, label, ticker: str, test_year: int, window: int = 240):
+def create_stock_data_LSTM_Intraday_3f(df_open, df_close, label, ticker: str, test_year: int, window: int = 240):
     df = pd.DataFrame()
     df['Date'] = df_close['Date']
     df['Name'] = ticker
@@ -69,7 +67,7 @@ def create_stock_data_Intraday_3f(df_open, df_close, label, ticker: str, test_ye
     test = df[df['Year'] == test_year].drop(columns=['Month', 'Year'])
     return np.array(train), np.array(test)
 
-def create_stock_data_Intraday_1f(df_open, df_close, label, ticker: str, test_year: int, window: int = 240):
+def create_stock_data_LSTM_Intraday_1f(df_open, df_close, label, ticker: str, test_year: int, window: int = 240):
     df = pd.DataFrame()
     df['Date'] = df_close['Date']
     df['Name'] = ticker
@@ -95,7 +93,7 @@ def create_stock_data_Intraday_1f(df_open, df_close, label, ticker: str, test_ye
     test = df[df['Year'] == test_year].drop(columns=['Month', 'Year'])
     return np.array(train), np.array(test)
 
-def create_stock_data_NextDay_1f(df_close, label, ticker: str, test_year: int, window: int = 240):
+def create_stock_data_LSTM_NextDay_1f(df_close, label, ticker: str, test_year: int, window: int = 240):
     df = pd.DataFrame()
     df['Date'] = df_close['Date']
     df['Name'] = ticker
@@ -120,3 +118,39 @@ def create_stock_data_NextDay_1f(df_close, label, ticker: str, test_year: int, w
     train = df[df['Year'] < test_year].drop(columns=['Month', 'Year'])
     test = df[df['Year'] == test_year].drop(columns=['Month', 'Year'])
     return np.array(train), np.array(test)
+
+def create_label_RF_Intraday(df_open, df_close, perc=[0.5, 0.5]):
+    if not np.all(df_close['Date'] == df_open['Date']):
+        raise ValueError('Date index mismatch')
+
+    perc = [0.] + list(np.cumsum(perc))
+    label = (df_close.iloc[:, 1:] / df_open.iloc[:, 1:] - 1).apply(
+        lambda x: pd.qcut(x.rank(method='first'), perc, labels=False), axis=1)
+
+    return label
+
+def create_stock_data_RF_Intraday_3f(df_open, df_close, label, ticker: str, test_year: int, window: int = 240):
+    df = pd.DataFrame([])
+    df['Date'] = list(df_close['Date'])
+    df['Name'] = ticker
+
+    daily_change = df_close[ticker] / df_open[ticker] - 1
+    m = list(range(1, 20)) + list(range(20, 241, 20))
+
+    for k in m:
+        df['IntraR'+str(k)] = daily_change.shift(k)
+    for k in m:
+        df['CloseR'+str(k)] = df_close[ticker].pct_change(k).shift(1)
+    for k in m:
+        df['OverNR'+str(k)] = df_open[ticker]/df_close[ticker].shift(k)-1
+
+    df['R-future'] = daily_change
+    df['label'] = list(label[ticker])
+    df['Month'] = list(df_close['Date'].str[:-3])
+
+    df = df.dropna()
+    trade_year = df['Month'].str[:4]
+    df = df.drop(columns=['Month'])
+    train_data = df[trade_year < str(test_year)]
+    test_data = df[trade_year == str(test_year)]
+    return np.array(train_data), np.array(test_data)
