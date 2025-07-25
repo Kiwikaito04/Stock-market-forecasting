016c709bbe3540794fb5e9ab5717fcf35f101af8
Kiwikaito <nguyenphamdieu41@gmail.com>
2025-07-25 19:55:01 +0700

    add note

--- src/simulate.py
+++ src/simulate.py
@@ -1,6 +1,19 @@
 import pandas as pd
 import numpy as np
 
+
+# Đầu ra mô hình: danh sách xác xuất cổ phiếu tăng (theo thứ tự cổ phiếu ban đầu
+
+
+# top_preds = [0, 2]  # Mua CP0 và CP2
+# trans_long = [0.02, 0.01]
+# → Long return = (0.02 + 0.01)/2 = 0.015
+
+# worst_preds = [1, 3]  # Bán khống CP1 và CP3
+# trans_short = -[-0.01, -0.03] = [0.01, 0.03]
+# → Short return = (0.01 + 0.03)/2 = 0.02
+
+
 # k: số lượng top cổ phiếu uptrend và downtrend
 def simulate(test_data, predictions, k=10):
     rets = pd.DataFrame([], columns=['Long', 'Short'])
